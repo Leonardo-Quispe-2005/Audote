e33967eca55587e6ec0dc0e17e8a84a7aebe2e94
Leonardo-Quispe-2005 <leonardoquispe633@gmail.com>
2023-09-13 20:22:09 -0300

    Projeto TCC

--- Audote-main/quemSomos.php
+++ Audote-main/quemSomos.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/34e911297d.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="src/style/pgprincipal.css" />
     <link rel="stylesheet" href="src/style/quemSomos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     
@@ -11,8 +15,10 @@
     <title>Quem Somos</title>
 </head>
 
-<body>
-    
+<body class="audote-root">
+
+    <?php include('./secoes/nav.php') ?>
+
     <section>
         <section>
 
@@ -219,6 +225,9 @@
 
     </section>    
 
+    <script src="src/js/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="src/js/pgprincipal.js"></script>
 </body>
 
 </html>
