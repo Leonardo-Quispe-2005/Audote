9a6bd9cd4408e5d2bfc63b376f1322536939bb8b
Leonardo-Quispe-2005 <leonardoquispe633@gmail.com>
2023-09-14 16:33:45 -0300

    Projeto TCC - Audote

--- Audote-main/quemSomos.php
+++ Audote-main/quemSomos.php
@@ -21,10 +21,9 @@
 
     <section>
         <section>
-
             <picture>
                 <div>
-                    <img src="src/img/dog.jpg" alt="" class="cachorro">
+                    <img src="src/img/dog2.png" alt="" class="cachorro">
                 </div>
             </picture>
         </section>
@@ -106,16 +105,16 @@
                         <div class="card">
                             <div class="content">
                                 <div class="img">
-                                    <img src="src/img/eu.jpg" alt="">
+                                    <img src="src/img/devs/Beatriz.jpg" alt="">
                                 </div>
                                 <div class="details">
-                                    <div class="name">Leonardo Quispe</div>
+                                    <div class="name">Beatriz</div>
                                     <div class="job">Web Designer</div>
                                 </div>
                                 <div class="media-icons">
-                                    <a href="#"><i class="bi bi-github"></i></a>
+                                    <a href="https://github.com/Biihz"><i class="bi bi-github"></i></a>
                                     <a href="#"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#"><i class="bi bi-instagram"></i></a>
+                                    <a href="https://www.instagram.com/_bea.zinha/"><i class="bi bi-instagram"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -124,16 +123,16 @@
                         <div class="card">
                             <div class="content">
                                 <div class="img">
-                                    <img src="src/img/eu.jpg" alt="">
+                                    <img src="src/img/devs/Bruno.jpg" alt="">
                                 </div>
                                 <div class="details">
-                                    <div class="name">Leonardo Quispe</div>
+                                    <div class="name">Bruno</div>
                                     <div class="job">Web Designer</div>
                                 </div>
                                 <div class="media-icons">
-                                    <a href="#"><i class="bi bi-github"></i></a>
+                                    <a href="https://github.com/BrunoGCSousa"><i class="bi bi-github"></i></a>
                                     <a href="#"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#"><i class="bi bi-instagram"></i></a>
+                                    <a href="https://www.instagram.com/brunogonziales/"><i class="bi bi-instagram"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -142,16 +141,16 @@
                         <div class="card">
                             <div class="content">
                                 <div class="img">
-                                    <img src="src/img/eu.jpg" alt="">
+                                    <img src="src/img/devs/Larissa.jpg" alt="">
                                 </div>
                                 <div class="details">
-                                    <div class="name">Leonardo Quispe</div>
+                                    <div class="name">Larissa</div>
                                     <div class="job">Web Designer</div>
                                 </div>
                                 <div class="media-icons">
-                                    <a href="#"><i class="bi bi-github"></i></a>
+                                    <a href="https://github.com/cattslari"><i class="bi bi-github"></i></a>
                                     <a href="#"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#"><i class="bi bi-instagram"></i></a>
+                                    <a href="https://www.instagram.com/cattslari/"><i class="bi bi-instagram"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -163,16 +162,16 @@
                         <div class="card">
                             <div class="content">
                                 <div class="img">
-                                    <img src="src/img/eu.jpg" alt="">
+                                    <img src="src/img/devs/Leonardo.jpg" alt="">
                                 </div>
                                 <div class="details">
                                     <div class="name">Leonardo Quispe</div>
                                     <div class="job">Web Designer</div>
                                 </div>
                                 <div class="media-icons">
-                                    <a href="#"><i class="bi bi-github"></i></a>
+                                    <a href="https://github.com/Leonardo-Quispe-2005"><i class="bi bi-github"></i></a>
                                     <a href="#"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#"><i class="bi bi-instagram"></i></a>
+                                    <a href="https://www.instagram.com/leonardo_quispe/"><i class="bi bi-instagram"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -181,16 +180,16 @@
                         <div class="card">
                             <div class="content">
                                 <div class="img">
-                                    <img src="src/img/eu.jpg" alt="">
+                                    <img src="src/img/devs/Lucas.jpg" alt="">
                                 </div>
                                 <div class="details">
-                                    <div class="name">Leonardo Quispe</div>
+                                    <div class="name">Lucas Cancela</div>
                                     <div class="job">Web Designer</div>
                                 </div>
                                 <div class="media-icons">
-                                    <a href="#"><i class="bi bi-github"></i></a>
+                                    <a href="https://github.com/LuCancela"><i class="bi bi-github"></i></a>
                                     <a href="#"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#"><i class="bi bi-instagram"></i></a>
+                                    <a href="https://www.instagram.com/cancelalucas/"><i class="bi bi-instagram"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -199,16 +198,16 @@
                         <div class="card">
                             <div class="content">
                                 <div class="img">
-                                    <img src="src/img/eu.jpg" alt="">
+                                    <img src="src/img/devs/Marcus.jpg" alt="">
                                 </div>
                                 <div class="details">
-                                    <div class="name">Leonardo Quispe</div>
+                                    <div class="name">Marcus Vinicius</div>
                                     <div class="job">Web Designer</div>
                                 </div>
                                 <div class="media-icons">
-                                    <a href="#"><i class="bi bi-github"></i></a>
+                                    <a href="https://github.com/LordMaquins"><i class="bi bi-github"></i></a>
                                     <a href="#"><i class="bi bi-linkedin"></i></a>
-                                    <a href="#"><i class="bi bi-instagram"></i></a>
+                                    <a href="https://www.instagram.com/marquinsvv/"><i class="bi bi-instagram"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -223,7 +222,84 @@
             </div>          
         </section>
 
-    </section>    
+    </section> 
+    
+    <!-- Footer -->
+    <footer class="site-footer style-1 mt-100" id="footer" style="background-image: url(src/img/bg10.png)">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <h5 class="footer-title">
+                        Sobre nós | Audote + Auqmia
+                    </h5>
+                    <div class="col">
+                        <div class="widget widget_about">
+                            <p>
+                                Somos <strong>Audote</strong>, e esse é o
+                                nosso site dedicado à adoção de pets! Somos
+                                um grupo de estudantes de desenvolvimento de
+                                sistemas apaixonados por ajudar animais de
+                                rua e promover a adoção responsável. Nosso
+                                principal propósito é contribuir com uma
+                                organização não governamental (ONG) que se
+                                dedica ao resgate e cuidado de animais
+                                abandonados, oferecendo uma plataforma para
+                                conectar esses animais a lares amorosos.
+                                <br />
+                                <br />
+
+                                Acreditamos que todos os animais merecem uma
+                                segunda chance e uma vida repleta de amor e
+                                cuidado. Por isso, decidimos unir nossas
+                                habilidades em desenvolvimento de sistemas
+                                para criar um espaço online onde você possa
+                                conhecer e adotar animais que estão
+                                aguardando por um lar afetuoso.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="widget widget_about">
+                            <p>
+                                Estamos orgulhosos de sermos parceiros da
+                                ONG AUQMIA, uma organização comprometida com
+                                a proteção e bem-estar dos animais. Juntos,
+                                trabalhamos em estreita colaboração para
+                                fornecer uma plataforma confiável e segura,
+                                onde você possa encontrar informações
+                                detalhadas sobre cada animal disponível para
+                                adoção, incluindo sua história,
+                                características e necessidades especiais, se
+                                houver. <br />
+                                <br />
+
+                                Ao adotar um pet através do nosso site, você
+                                não apenas traz felicidade para sua vida,
+                                mas também contribui diretamente para a
+                                missão da nossa parceria com a AUQMIA e
+                                ajuda a salvar mais animais de rua. Cada
+                                adoção faz a diferença e nos aproxima de um
+                                mundo onde todos os animais tenham um lar
+                                amoroso.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer bottom part -->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 text-center">
+                        <span class="copyright-text">Copyright © 2023
+                            <a href="#" target="_blank">AUDOTE</a>. Todos
+                            direitos Reservados.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <script src="src/js/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
